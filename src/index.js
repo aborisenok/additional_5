07fbaeda4bd0878a391f5e2d70674aa9f4ac3375
aborisenok <lesha_borisenok@mail.ru>
2018-09-12 14:05:11 +0300

    well done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  var brackArr = str.split("");
+  var seq = [];
+
+  for(let i = 0; i < brackArr.length; i++){
+    if(i > 0){
+      if(compare(brackArr[i], seq[seq.length-1], bracketsConfig)){
+        seq.pop();
+      }else{
+        seq.push(brackArr[i]);
+      }
+    }else{
+         seq.push(brackArr[i]);
+    }
+  }
+  return seq.length === 0 ? true : false;
+}
+
+
+function compare(val1, val2, bracketsConfig){
+  var isPair = false;
+  bracketsConfig.forEach(function(item){
+    if((val2 == item[0] && val1 == item[1])){
+      isPair = true;
+    }
+  });
+  return isPair;
 }
