0d84edf158cd8c7975bdf38100c2efe13e9590fb
aborisenok <lesha_borisenok@mail.ru>
2018-09-30 21:27:07 +0300

    refactoring

--- src/index.js
+++ src/index.js
@@ -1,26 +1,26 @@
 module.exports = function check(str, bracketsConfig) {
-  var brackArr = str.split("");
-  var seq = [];
+  const brackets = str.split("");
+  let sequence = [];
 
-  for(let i = 0; i < brackArr.length; i++){
+  for(let i = 0; i < brackets.length; i++){
     if(i > 0){
-      if(compare(brackArr[i], seq[seq.length-1], bracketsConfig)){
-        seq.pop();
+      if(compare(sequence[sequence.length-1], brackets[i], bracketsConfig)){
+        sequence.pop();
       }else{
-        seq.push(brackArr[i]);
+        sequence.push(brackets[i]);
       }
     }else{
-         seq.push(brackArr[i]);
+      sequence.push(brackets[i]);
     }
   }
-  return seq.length === 0 ? true : false;
+  return sequence.length === 0 ? true : false;
 }
 
 
-function compare(val1, val2, bracketsConfig){
-  var isPair = false;
+function compare(firstBracket, secondBracket, bracketsConfig){
+  let isPair = false;
   bracketsConfig.forEach(function(item){
-    if((val2 == item[0] && val1 == item[1])){
+    if((firstBracket === item[0] && secondBracket === item[1])){
       isPair = true;
     }
   });
